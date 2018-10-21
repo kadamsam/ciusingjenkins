a1a50f2fe0a4d284a4ec32a4bb14319dae55efcc
kadamsam <sameerkadam99@gmail.com>
2018-10-21 18:55:34 +0530

    Update master_script.sh

--- master_script.sh
+++ master_script.sh
@@ -4,9 +4,6 @@
 USERNAME=ec2-user
 HOSTS="ec2-13-56-149-218.us-west-1.compute.amazonaws.com"
 SCRIPT="sh install/install.sh"
-ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+ssh -i key/aws-sameer-uswest-1.ppk -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 
-
-#  Set up CI to fetch the codebase from https://github.com/ahfarmer/emoji-search.git  and package it for deployment.
-
-#  Deploy and launch the application in dev environment for every new commit or every 6 hours.
+#  Push the 'Jenkinsfile' to Jenkin master
